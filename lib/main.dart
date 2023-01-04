14e98076747204317f4f0619c3ca53a44456e9f9
Momen Muhammad <momenmuhammad01@gmail.com>
2023-01-04 18:19:33 +0200

    important init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notely/app.dart';
+import 'package:notely/core/shared/bloc_observer.dart';
+import 'package:notely/core/shared/service_locator.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  Bloc.observer = MyBlocObserver();
+  await ServiceLocator().init();
 }
